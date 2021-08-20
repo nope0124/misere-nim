564cdb3d3e0143be225a8b5e38d051241aff081b
nope0124 <kami-ryo0124@ezweb.ne.jp>
2021-08-20 09:24:21 +0900

    [Fix] UI

---
+++ Assets/Scripts/LineScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineScript : MonoBehaviour
+{
+    public int colorType;
+    public Material frameColor;
+    public Material defaultColor;
+
+    void Start () {
+        LineRenderer renderer = gameObject.GetComponent<LineRenderer>();
+        renderer.SetVertexCount(5);
+        renderer.SetPosition(0, new Vector3(7.0f + 15f * (colorType - 1), 0.6f, 12.0f));
+        renderer.SetPosition(1, new Vector3(-7.0f + 15f * (colorType - 1), 0.6f, 12.0f));
+        renderer.SetPosition(2, new Vector3(-7.0f + 15f * (colorType - 1), 0.6f, -2.0f));
+        renderer.SetPosition(3, new Vector3(7.0f + 15f * (colorType - 1), 0.6f, -2.0f));
+        renderer.SetPosition(4, new Vector3(7.0f + 15f * (colorType - 1), 0.6f, 12.0f));
+    }
+
+    void Update()
+    {
+        LineRenderer renderer = gameObject.GetComponent<LineRenderer>();
+        if (MainControllerScript.getCurCount() == colorType) {
+            renderer.material = frameColor;
+            renderer.SetWidth(0.4f, 0.4f);
+        } else {
+            renderer.material = defaultColor;
+            renderer.SetWidth(0.2f, 0.2f);
+        }
+    }
+}
---
+++ Assets/Scripts/RotateScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateScript : MonoBehaviour
+{
+    Vector3 start;
+    Vector3 end;
+    Vector3 vecStart = new Vector3(1, 1, 1);
+    Vector3 vecEnd = new Vector3(2, 2, 2);
+    bool moving = false;
+    float timer = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 2, 0, Space.World);
+        if (moving) {
+            float locationTime = (Time.time - timer) / 0.1f;
+            transform.localScale = Vector3.Lerp(vecStart, vecEnd, locationTime);
+            transform.position = Vector3.Lerp(start, end, locationTime);
+            if (locationTime >= 1.0f) moving = false;
+        }
+    }
+
+    void toSmall() {
+        if (transform.localScale != new Vector3(1, 1, 1)) {
+            moving = true;
+            vecStart = new Vector3(2, 2, 2);
+            vecEnd = new Vector3(1, 1, 1);
+            start = new Vector3(transform.position.x, 3.5f, transform.position.z);
+            end = new Vector3(transform.position.x, 2.0f, transform.position.z);
+            timer = Time.time;
+        }
+    }
+
+    void toBig() {
+        if (transform.localScale != new Vector3(2, 2, 2)) {
+            moving = true;
+            vecStart = new Vector3(1, 1, 1);
+            vecEnd = new Vector3(2, 2, 2);
+            start = new Vector3(transform.position.x, 2.0f, transform.position.z);
+            end = new Vector3(transform.position.x, 3.5f, transform.position.z);
+            timer = Time.time;
+        }
+    }
+}
